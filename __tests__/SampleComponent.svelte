659db1de5293ec244b921ee222ecea5f19e7571b
callakkumar <karthiknetworklink@gmail.com>
2022-07-18 07:45:22 +0000

    First Revision

---
+++ __tests__/SampleComponent.svelte
@@ -0,0 +1,7 @@
+<!--
+ Copyright (c) 2022 KrestUI
+ 
+ This software is released under the MIT License.
+ https://opensource.org/licenses/MIT
+-->
+<div>sample</div>
---
+++ src/lib/index.js
@@ -0,0 +1,6 @@
+// Copyright (c) 2022 KrestUI
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+export { default as ContextTest } from './ContextTest.svelte';
